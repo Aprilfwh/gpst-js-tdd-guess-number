09de7df07b9fcbd6c5a74c9facb322bbd80989e1
fanfan123 <1820148513@qq.com>
2018-04-26 17:52:39 +0800

    GuessNumber

--- main/main.js
+++ main/main.js
@@ -1,6 +1,28 @@
 // Write your cade below:
-function main() {};
-
-module.exports = {
-    main
+function GuessNumber(inputs) {
+    function Compare(input1,input2) {
+        if(input1===input2){
+            return (input1.length.toString()) + "A0B";
+        }
+        var a_num = 0;//统计数字位置都猜对的数量
+        var b_num=0;//统计数字才对位置不对的数量
+        for(var i=0;i<input1.length;i++){
+            for(var j=0;j<input2.length;j++){
+                if(input2[j]===input1[i]){
+                    if(j===i){
+                        a_num += 1;
+                    }
+                    else {
+                        b_num += 1;
+                    }
+                    break;
+                }
+            }
+        }
+        return (a_num.toString()) + 'A' + (b_num.toString()) +'B';
+    }
+    var result = Compare(inputs[0], inputs[1]);
+    console.log(result);
+    return result;
 }
+module.exports=GuessNumber;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,14 @@
-const main = require('../main/main');
+var GuessNumber = require('../main/main.js');
 
-describe('main()', () => {
-
+describe('guess number()', function(){
+    it('数字猜对位置也对',function () {
+        let inputs =["1234", "1234"];
+        let result = GuessNumber(inputs);
+        expect(result).toEqual("4A0B");
+    });
+    it('数字猜对位置不对',function () {
+        let inputs =["1234", "4321"];
+        let result = GuessNumber(inputs);
+        expect(result).toEqual("0A4B");
+    });
 });
